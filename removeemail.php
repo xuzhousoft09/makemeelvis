116e8e58605ca087805235a7adb0c700e5d03826
qiang.lv <xzsdeveloper@gmail.com>
2019-01-13 21:30:17 +0800

    delete the removeemail.html

--- removeemail.php
+++ removeemail.php
@@ -7,21 +7,37 @@
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
+  <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+  <img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+  <p>Please select the email addresses to delete from the email list and click Remove.</p>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
 <?php
   $dbc = mysqli_connect('localhost', 'root', '', 'elvis_store')
     or die('Error connecting to MySQL server.');
-
-  $email = $_POST['email'];
-
-  $query = "DELETE FROM email_list WHERE email = '$email'";
-  mysqli_query($dbc, $query)
-    or die('Error querying database.');
-
-  echo 'Customer removed: ' . $email;
-
+  // Delete the customer rows (only if the form has been submitted)
+  if (isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $delete_id) {
+      $query = "DELETE FROM email_list WHERE id = $delete_id";
+      mysqli_query($dbc, $query)
+        or die('Error querying database.');
+    } 
+    echo 'Customer(s) removed.<br />';
+  }
+  // Display the customer rows with checkboxes for deleting
+  $query = "SELECT * FROM email_list";
+  $result = mysqli_query($dbc, $query);
+  while ($row = mysqli_fetch_array($result)) {
+    echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]" />';
+    echo $row['first_name'];
+    echo ' ' . $row['last_name'];
+    echo ' ' . $row['email'];
+    echo '<br />';
+  }
   mysqli_close($dbc);
 ?>
 
+    <input type="submit" name="submit" value="Remove" />
+  </form>
 </body>
 </html>
